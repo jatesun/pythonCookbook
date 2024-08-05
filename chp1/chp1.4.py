12146f486234376a752072f625b9d66097464f05
jatesun <jatesun111@gmail.com>
2024-08-05 15:00:33 +0800

    2024年08月05日 chp1.4

---
+++ chp1/chp1.4.py
@@ -0,0 +1,26 @@
+"""
+查找最大或最小的n个元素
+heapq 模块有两个函数：nlargest() 和 nsmallest() 可以完美解决这个问题。
+"""
+import heapq
+
+nums = [1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2]
+print(heapq.nlargest(3, nums))
+print(heapq.nsmallest(3, nums))
+
+# 两个函数都能接受一个关键字参数，用于更复杂的数据结构中：
+
+portfolio = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(3, portfolio, key=lambda s: s['price'])
+print(cheap)
+print(expensive)
+
+
