a69dfb70550dc5baf10f10bff3760d0e64737e4a
jatesun <jatesun111@gmail.com>
2024-08-05 15:17:14 +0800

    2024年08月05日 chp1.5

---
+++ chp1/chp1.5.py
@@ -0,0 +1,18 @@
+"""
+怎样实现一个按优先级排序的队列？ 并且在这个队列上面每次 pop 操作总是返回优先级最高的那个元素
+下面的类利用 heapq 模块实现了一个简单的优先级队列：
+"""
+import heapq
+
+
+class priorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
